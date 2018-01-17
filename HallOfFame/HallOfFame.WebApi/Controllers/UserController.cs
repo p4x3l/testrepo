da9833a7000c63bbb487fc3efbbff4fb272f1347
David <david@mcom.se>
2018-01-17 14:18:59 +0100

    Add files via upload

---
+++ HallOfFame/HallOfFame.WebApi/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using HallOfFame.WebApi.Contracts;
+using HallOfFame.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HallOfFame.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        private IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [ResponseCache(NoStore = true, Duration = 0)]
+        [HttpGet("")]
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            var test = await _userService.GetAll();
+            return test;
+        }
+
+        [ResponseCache(NoStore = true, Duration = 0)]
+        [HttpGet("{id}")]
+        public async Task<User> Get(string id)
+        {
+            var user = await _userService.Get(id);
+            return user;
+        }
+
+        [ResponseCache(NoStore = true, Duration = 0)]
+        [HttpGet("current")]
+        public async Task<User> GetCurrentUser()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+
+            var user = await _userService.Get(claimsIdentity.Name);
+            return user;
+        }
+    }
+}
---
+++ HallOfFame/HallOfFame.WebApi/Models/AuthorizeUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HallOfFame.WebApi.Models
+{
+    public class AuthorizeUserModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
